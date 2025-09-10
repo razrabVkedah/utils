cb7919144b3087afa3e750c2be495fc2420d6f4b
razrabVkedah <139863980+bullrusleo@users.noreply.github.com>
2025-09-10 08:31:12 +0300

    Add Logging, Hotkeys and ButtonAttribute

---
+++ Editor/InspectorButtons/GlobalButtonEditorForMB.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Rusleo.Utils.Editor.InspectorButtons
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(MonoBehaviour), true, isFallback = true)]
+    public class GlobalButtonEditorForMB : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            GlobalButtonEditor.DrawButtons(targets, target.GetType());
+        }
+    }
+}
---
+++ Editor/InspectorButtons/GlobalButtonEditorForSO.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Rusleo.Utils.Editor.InspectorButtons
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(ScriptableObject), true, isFallback = true)]
+    public class GlobalButtonEditorForSO : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            GlobalButtonEditor.DrawButtons(targets, target.GetType());
+        }
+    }
+}

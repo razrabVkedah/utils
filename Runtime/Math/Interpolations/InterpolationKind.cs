3f416d4de3b7c287657990b123f3114d316838d4
bullrusleo <leonrusgamedev2004@yandex.ru>
2025-09-12 00:08:13 +0300

    interpolations + splines

---
+++ Runtime/Math/Interpolations/InterpolationKind.cs
@@ -0,0 +1,23 @@
+﻿namespace Rusleo.Utils.Runtime.Math.Interpolations
+{
+    /// <summary>
+    /// Типы интерполяции между скалярными значениями.
+    /// </summary>
+    public enum InterpolationKind
+    {
+        /// <summary>
+        /// линейная
+        /// </summary>
+        Lerp,
+
+        /// <summary>
+        /// требует касательные (m0, m1)
+        /// </summary>
+        CubicHermite,
+
+        /// <summary>
+        /// сплайн через точки, касательные вычисляются автоматически
+        /// </summary>
+        CatmullRom
+    }
+}
--- Runtime/Noise/INoise.cs
+++ Runtime/Noise/INoise.cs
@@ -10,6 +10,7 @@ namespace Rusleo.Utils.Runtime
         // Start is called before the first frame update
         void Start()
         {
+            
         }
 
         // Update is called once per frame

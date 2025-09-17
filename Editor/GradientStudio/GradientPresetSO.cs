9f9dfaa0594b5e69d2f14cbb090ee1935c1ee4ef
bullrusleo <leonrusgamedev2004@yandex.ru>
2025-09-17 09:13:08 +0300

    feat: gradient studio (base)

---
+++ Editor/GradientStudio/GradientPresetSO.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Rusleo.Utils.Editor.GradientStudio
+{
+    public class GradientPresetSO : ScriptableObject
+    {
+        public Gradient gradient = new Gradient();
+    }
+}

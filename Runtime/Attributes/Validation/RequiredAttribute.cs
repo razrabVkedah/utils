9f5465f048f556afee384a48c708c7f1279d360b
bullrusleo <leonrusgamedev2004@yandex.ru>
2026-03-30 23:55:33 +0300

    required attribute

---
+++ Runtime/Attributes/Validation/RequiredAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Rusleo.Utils.Runtime.Attributes.Validation
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public sealed class RequiredAttribute : PropertyAttribute
+    {
+        public RequiredAttribute(string message = null)
+        {
+            Message = string.IsNullOrWhiteSpace(message)
+                ? "Required reference is missing."
+                : message;
+        }
+
+        public string Message { get; }
+    }
+}
